60cff6c652ed096f2ab414d80be39028180dcb99
Stepan <stepan-pl@protonmail.com>
2022-04-17 05:33:41 +0300

    Update db.sql
    добавил индексы

--- schema/db.sql
+++ schema/db.sql
@@ -1,6 +1,9 @@
 CREATE TABLE shortener
 (
-    id       serial PRIMARY KEY,
-    short    VARCHAR(100)  NOT NULL,
-    original VARCHAR(200) NOT NULL
-)
+    id       serial,
+    short    VARCHAR(100),
+    original VARCHAR(200)
+);
+
+CREATE INDEX idx_shortener_short_original ON shortener(short, original); 
+CREATE INDEX idx_shortener_original ON shortener(original); 
